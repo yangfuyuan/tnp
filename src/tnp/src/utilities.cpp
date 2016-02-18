39240a27177d77bbac8224f40cd65c70c1361bd5
Fotis <fbalampanis@gmail.com>
2016-02-18 14:52:18 +0100

    code cleaning

---
+++ src/tnp/src/utilities.cpp
@@ -0,0 +1,7 @@
+#include "utilities.h"
+
+Utilities::Utilities()
+{
+
+}
+
---
+++ src/tnp/src/utilities.h
@@ -0,0 +1,15 @@
+#ifndef UTILITIES_H
+#define UTILITIES_H
+
+
+class Utilities
+{
+public:
+    Utilities();
+
+signals:
+
+public slots:
+};
+
+#endif // UTILITIES_H
